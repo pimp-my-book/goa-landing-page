739831d2e86e8b4871fa82e1ad1a73e7ca493321
amodinho <u14284783@tuks.co.za>
2019-04-30 11:51:19 +0200

    Started with Navigation Bar

---
+++ src/components/Navigation/NavigationBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+        <div class="flex items-center flex-no-shrink text">
+            <span>GOA</span>
+        </div>
+    </nav>
+</template>
