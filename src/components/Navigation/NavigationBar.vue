67aad3e05918fec7874522de5f96412c25dde838
amodinho <u14284783@tuks.co.za>
2019-04-30 12:47:32 +0200

    Added the navigation Items

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -6,6 +6,19 @@
         <div class="block lg:hidden">
             <span class="flex items-center px-3 py-2">MENU</span>
         </div>
+        <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-sm lg:flex-grow">
+                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    How It Works
+                </a>
+                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    Contact
+                </a>
+                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    Support
+                </a>
+            </div>
+        </div>
     </nav>
 </template>
 <script>
