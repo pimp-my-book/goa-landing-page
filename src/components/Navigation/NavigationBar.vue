09ebb98e39d053e23371115d4fbc5e35fb73e3d9
amodinho <u14284783@tuks.co.za>
2019-04-30 13:01:48 +0200

    Fixed navigation menu

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -1,21 +1,21 @@
 <template>
     <nav class="border-top flex items-center justify-between flex-wrap bg-green-darker p-6">
         <div class="flex items-center flex-no-shrink text">
-            <span class="font-display text-white">GOA</span>
+            <h1 class="font-display text-white">GOA</h1>
         </div>
         <div class="block lg:hidden">
             <span class="flex items-center px-3 py-2">MENU</span>
         </div>
         <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                   <p> How It Works</p>
+                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                   <p class="font-body"> How It Works</p>
                  </a>
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                    <p>Contact</p>
+                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    <p class="font-body">Contact</p>
                 </a>
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                    <p>Support</p>
+                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    <p class="font-body">Support</p>
                 </a>
             </div>
             <div>
