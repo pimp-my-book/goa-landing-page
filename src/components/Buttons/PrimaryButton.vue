f6110440584dfe20f6e55d9c152690e7469b596d
amodinho <u14284783@tuks.co.za>
2019-04-30 12:58:23 +0200

    Change the typography types

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -1,5 +1,15 @@
 <template>
     <button class="bg-green font-body text-white  py-2 px-4 rounded">
-     Button Text
+     {{text}}
     </button>
 </template>
+
+<script>
+export default {
+    name: 'PrimaryButton',
+    props: {
+        text: String
+    }
+}
+</script>
+
--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -9,17 +9,20 @@
         <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
                 <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                    How It Works
+                   <p> How It Works</p>
+                 </a>
+                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                    <p>Contact</p>
                 </a>
                 <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                    Contact
-                </a>
-                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
-                    Support
+                    <p>Support</p>
                 </a>
             </div>
             <div>
-               <PrimaryButton/>
+                <p class="text-white font-body mr-4">Login</p>
+            </div>
+            <div>
+               <PrimaryButton text="Sign Up"/>
             </div>
         </div>
     </nav>
