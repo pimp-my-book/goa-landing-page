bcba867599146bb8eb045d47c4b5bf96ddae6e56
amodinho <u14284783@tuks.co.za>
2019-04-30 12:00:19 +0200

    Removing boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div class="bg-purple text-white xl:bg-orange">
-      Test
-    </div>
+   
    
   </div>
 </template>
