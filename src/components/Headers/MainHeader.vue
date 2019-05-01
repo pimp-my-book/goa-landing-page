fd5a7edae2674e6a76cfb0453e1a14a1e885ac5e
amodinho <u14284783@tuks.co.za>
2019-05-01 13:46:55 +0200

    ADD background coverr settings

--- src/components/Headers/MainHeader.vue
+++ src/components/Headers/MainHeader.vue
@@ -15,6 +15,14 @@ export default{
      position: absolute;
      width: 90vw;
      height: 729px;
+     background: url('');
+     background-size: cover;
+     background-repeat: no-repeat;
+     background-position: center center;
+     background-attachment: fixed;
+     -webkit-background-size:cover;
+     -moz-background-size:cover;
+     -o-background-size:cover;
  }
 
 </style>
