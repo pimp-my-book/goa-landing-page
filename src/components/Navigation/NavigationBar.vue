7b687c2a76dff81f814a5f714bc0724b1634c81d
amodinho <u14284783@tuks.co.za>
2019-05-01 12:38:19 +0200

    Finally fixed the nav bar

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -3,25 +3,25 @@
         <div class="flex items-center flex-no-shrink text">
             <h1 class="font-display text-white">GOA</h1>
         </div>
-        <div class="block lg:hidden">
+        <div class="block sm:hidden">
             <span @click="toggle" class="flex items-center px-3 py-2">MENU</span>
         </div>
-        <div :class="open ? 'block':'hidden'" class="w-full lg:flex-grow lg:flex lg:items-center lg:w-auto ">
-            <div class=" text-sm md:flex-grow ">
-                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+        <div :class="open ? 'block':'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto ">
+            <div class=" text-md sm:flex-grow ">
+                <a href="#" class="block mt-4 no-underline sm:inline-block sm:mt-0 text-white hover:text-bg-green-light mr-4">
                    <p class="font-body"> How It Works</p>
                  </a>
-                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                <a href="#" class="block mt-4 no-underline sm:inline-block sm:mt-0 text-white hover:text-bg-green-light mr-4">
                     <p class="font-body">Contact</p>
                 </a>
-                <a href="#" class="block mt-4 no-underline lg:inline-block lg:mt-0 text-white hover:text-bg-green-light mr-4">
+                <a href="#" class="block mt-4 no-underline sm:inline-block sm:mt-0 text-white hover:text-bg-green-light mr-4">
                     <p class="font-body">Support</p>
                 </a>
             </div>
-            <div class="mt-4">
+            <div class="nav__drawer--right">
                 <p class="text-white font-body mr-4 ">Login</p>
             </div>
-            <div>
+            <div class="nav__drawer--right ">
                <PrimaryButton text="Sign Up"/>
             </div>
         </div>
@@ -47,4 +47,18 @@ export default {
     }
 }
 </script>
+
+<style >
+
+ @media (min-width: 400px){
+        .nav__drawer--right{
+            margin-top: 10px;
+   
+}
+
+
+    }
+
+</style>
+
 
