15780cec86a18d4760abc48d045785576a691800
amodinho <u14284783@tuks.co.za>
2019-05-02 12:30:10 +0200

    added a URL prop to the buttons

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -1,6 +1,9 @@
 <template>
     <button class="bg-green font-body text-white  py-2 px-4 rounded">
+     <a class="no-underline" 
+     :href="url">
      {{text}}
+     </a>
     </button>
 </template>
 
@@ -8,7 +11,8 @@
 export default {
     name: 'PrimaryButton',
     props: {
-        text: String
+        text: String,
+        url: String
     }
 }
 </script>
--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -1,8 +1,17 @@
 <template>
-    
+    <button class="bg-green-light font-body text-white  py-2 px-4 rounded">
+     <a class="no-underline" 
+     :href="url">
+     {{text}}
+     </a>
+    </button>
 </template>
 <script>
 export default {
-    name: 'SecondaryButton'
+    name: 'SecondaryButton',
+    props: {
+        text: String,
+        url: String
+    }
 }
 </script>
