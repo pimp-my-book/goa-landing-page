8bb9c2bc24f75a5a6c86a31eb5f930e1d57c20f6
amodinho <u14284783@tuks.co.za>
2019-05-02 11:43:39 +0200

    Completed the Feature text component

---
+++ src/components/Typography/FeatureText.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-lg font-body">{{text}}</h1>
+</template>
+<script>
+export default {
+    name:'FeatureText',
+    props: {
+        text: String
+    }
+}
+</script>
