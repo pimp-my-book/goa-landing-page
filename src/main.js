8501fac59cfbf6ddce8e84b5855ba4eeb5c252dd
amodinho <u14284783@tuks.co.za>
2019-05-03 12:46:05 +0200

    Initilized Vue Router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import VueAnalytics from 'vue-analytics'
 import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
-
+Vue.use(VueRouter)
 Vue.use(VueAnalytics, {
   id: process.env.GA_ID
 })
