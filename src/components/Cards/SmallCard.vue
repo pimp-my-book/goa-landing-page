8897c159823efb126464cfaf2a1f10fa6003dd9e
amodinho <u14284783@tuks.co.za>
2019-05-03 13:54:20 +0200

    Made structure for card

--- src/components/Cards/SmallCard.vue
+++ src/components/Cards/SmallCard.vue
@@ -1,10 +1,17 @@
 <template>
-<div>
-    
+<div class="w-1/4 rounded">
+  <img class="" />
+  <div>
+   
+  </div>
+  <div>
+
+  </div>
 </div>
-    
 </template>
+
 <script>
+import 
 export default {
     name: 'SmallCard',
     props: {
