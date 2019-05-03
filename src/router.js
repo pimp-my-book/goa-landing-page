fdaace4a549e459e1e1815159eddf73edbf8bd4c
amodinho <u14284783@tuks.co.za>
2019-05-03 12:48:53 +0200

    Added a routing file

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+const routes = [
+    {path:'/', components: //Home}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
