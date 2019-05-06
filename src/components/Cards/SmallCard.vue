c957885d4de24da3780d59dd0986383327297637
amodinho <u14284783@tuks.co.za>
2019-05-06 10:25:23 +0200

    Added the styling for the card

--- src/components/Cards/SmallCard.vue
+++ src/components/Cards/SmallCard.vue
@@ -1,10 +1,13 @@
 <template>
-<div class="w-1/4 rounded">
+<div class="max-w-sm overflow-hidden shadow-lg w-1/4 rounded">
   <img class="" />
   <div>
    <DisplaySmall :text="cardTitle"/>
   </div>
   <div>
+   <FeatureText
+   :text="cardText"
+   />
 
   </div>
 </div>
@@ -12,6 +15,7 @@
 
 <script>
 import DisplaySmall from "../Typography/DisplaySmall"
+import FeatureText from "../Typography/FeatureText"
 
 export default {
     name: 'SmallCard',
