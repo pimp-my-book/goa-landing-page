49d938e132da81705e135125ea87b0f390989f79
amodinho <u14284783@tuks.co.za>
2019-05-10 15:03:25 +0200

    an href to the feature text component

--- src/components/Typography/FeatureText.vue
+++ src/components/Typography/FeatureText.vue
@@ -1,12 +1,19 @@
 <template>
-    <p :class="[small ? 'text-sm font-body' : 'text-lg font-body' ]">{{text}}</p>
+    <p :class="[small ? 'text-sm font-body' : 'text-lg font-body' ]">
+    <a
+    :href="url" 
+    class="no-underline">    
+    {{text}}
+    </a>
+    </p>
 </template>
 <script>
 export default {
     name:'FeatureText',
     props: {
         text: String,
-        small: Boolean
+        small: Boolean,
+        url: String 
     }
 }
 </script>
