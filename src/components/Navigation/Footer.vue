d814bf9ab07a2fed096d0d4eccf7d7db9a33bde1
amodinho <u14284783@tuks.co.za>
2019-05-10 15:00:17 +0200

    Got the correct position of the items

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -1,9 +1,12 @@
 <template>
     <footer class="bg-green-darker mt-32 w-full ">
-      <div class="flex flex-row">
+      <div class="container relative">
+          <div class="flex absolute mt-32 pt-32">
+           <p class="text-white">Hello</p>
           <p>Hello</p>
           <p>Hello</p>
-          <p>Hello</p>
+          </div>
+        
       </div>
     </footer>
 </template>
@@ -17,6 +20,10 @@ footer {
     clip-path: polygon(59% 21%, 100% 1%, 100% 100%, 0 100%, 0 49%);
     height: 500px;
     /*transform: matrix(0.98, -0.2, 0.21, 0.98,0,0);*/
+}
+
+.flex{
+    left:50vw;
 }
 </style>
 
