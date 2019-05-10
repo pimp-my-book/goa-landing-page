7eb201d8c21714e0a4d1e5b9a6d4d0cf63c8a7a1
amodinho <u14284783@tuks.co.za>
2019-05-10 11:14:22 +0200

    Added the fourth flex-row

--- src/components/Bodies/LandingBody.vue
+++ src/components/Bodies/LandingBody.vue
@@ -200,6 +200,75 @@
 
 
 
+  <div class=" sm:flex lg:flex-row sm:flex-col">
+<!-- Start of flex-row 4-->
+
+<!-- start of img for sm devices-->
+     <div>
+     <img 
+     class="w-img h-img lg:hidden"
+     alt="students" 
+     src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_empty_xct9.svg"
+     />
+   </div>
+<!-- End of img -->
+
+<!-- start of text-part of div -->
+<div class="lg:text-left lg:w-3/4 lg:pl-32 s:text-center">
+     <DisplaySmall
+   text="Your Students’ Needs Sorted"
+   class="mb-5 text-blue-dark"
+   />
+   <FeatureText
+   text="We created an easy to use experince that allows students to order what they need and have records of their order history. They also get notified about the stages of their order."
+   />
+  <!-- start of ul -->
+  <div class="lg:p-12 lg:text-left lg:pr-32">
+ <ul class="mx-auto">
+   <li>
+     <FeatureText
+   text="Order History"
+   
+   />
+   </li>
+   <li>
+     <FeatureText
+   text="Restriction on Book Orders"
+   />
+   </li>
+   <li>
+     <FeatureText
+   text="Get Books for all degrees"
+   />
+   </li>
+   <li>
+      <FeatureText
+   text="Delivery at their destination of choice"
+   />
+   </li>
+   </ul>
+          <!-- end of ul -->
+
+  </div>
+    
+ 
+     <!-- End of text-part of div -->
+   </div>
+
+<!-- start of img -->
+     <div>
+     <img 
+     class="w-img h-img  s:hidden lg:block"
+     alt="students" 
+     src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_empty_xct9.svg"
+     />
+   </div>
+<!-- End of img -->
+
+   </div>
+   <!-- end of flex-row 4 -->
+
+
 
 
  </div>
