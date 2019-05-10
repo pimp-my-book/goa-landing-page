6e65af20e339f9a7b8dca40a79b1a2390fbd0b99
amodinho <u14284783@tuks.co.za>
2019-05-10 11:00:07 +0200

    Added second Flex row

--- src/components/Bodies/LandingBody.vue
+++ src/components/Bodies/LandingBody.vue
@@ -2,7 +2,7 @@
      <div class="container ">
    <!-- Start of container -->
    <div class=" sm:flex lg:flex-row sm:flex-col">
-<!-- Start of flex-row -->
+<!-- Start of flex-row 1-->
 
 <!-- start of img -->
      <div>
@@ -62,9 +62,80 @@
    
    
    </div>
-   <!-- end of flex-row -->
+   <!-- end of flex-row 1 -->
+
+
+  <div class=" sm:flex lg:flex-row sm:flex-col">
+<!-- Start of flex-row 2-->
+
+<!-- start of img -->
+     <div>
+     <img 
+     class="w-img h-img"
+     alt="students" 
+     src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_wallet_aym5+(1).svg"
+     />
+   </div>
+<!-- End of img -->
+
+<!-- start of text-part of div -->
+<div class="lg:text-left lg:w-3/4 lg:pl-32 s:text-center">
+     <DisplaySmall
+   text="Your Students’ Needs Sorted"
+   class="mb-5 text-blue-dark"
+   />
+   <FeatureText
+   text="We created an easy to use experince that allows students to order what they need and have records of their order history. They also get notified about the stages of their order."
+   />
+  <!-- start of ul -->
+  <div class="lg:p-12 lg:text-left lg:pr-32">
+ <ul class="mx-auto">
+   <li>
+     <FeatureText
+   text="Order History"
+   
+   />
+   </li>
+   <li>
+     <FeatureText
+   text="Restriction on Book Orders"
+   />
+   </li>
+   <li>
+     <FeatureText
+   text="Get Books for all degrees"
+   />
+   </li>
+   <li>
+      <FeatureText
+   text="Delivery at their destination of choice"
+   />
+   </li>
+   </ul>
+          <!-- end of ul -->
+
+  </div>
+    
+ 
+     <!-- End of text-part of div -->
+   </div>
+
+
+
+
+   
+   
+   </div>
+   <!-- end of flex-row 2 -->
+
+
+
+
+
  </div>
    <!-- End  of container --> 
+
+
       
 </template>
 
