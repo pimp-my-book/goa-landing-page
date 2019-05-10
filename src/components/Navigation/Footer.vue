6b6c739be035cb47c343e02f82bb06455dc60a08
amodinho <u14284783@tuks.co.za>
2019-05-10 12:26:42 +0200

    Added flex row

--- src/components/Navigation/Footer.vue
+++ src/components/Navigation/Footer.vue
@@ -1,8 +1,18 @@
 <template>
-    
+    <footer class="bg-green-darker">
+      <div class="flex flex-row">
+          
+      </div>
+    </footer>
 </template>
 <script>
 export default {
     name:'Footer'
 }
 </script>
+<style>
+footer{
+    transform: matrix(0.98, -0.2, 0.21, 0.98,0,);
+}
+</style>
+
