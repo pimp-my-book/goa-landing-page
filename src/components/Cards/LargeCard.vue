13f63dd266131dbed52b30cf7cd33603df2c1a10
amodinho <u14284783@tuks.co.za>
2019-05-10 11:55:01 +0200

    Added the styling for the card

--- src/components/Cards/LargeCard.vue
+++ src/components/Cards/LargeCard.vue
@@ -1,8 +1,44 @@
 <template>
-    
+    <div class= "shadow-lg x:w-4/5 x:h-full LargeCard rounded-lg lg:mr-10">
+  <img 
+  class="mt-5"
+  :src="img"
+  alt="money"
+   />
+  <div>
+   <DisplaySmall 
+   class="mt-5 text-blue-dark"
+   :text="cardTitle"
+   />
+  </div>
+  <div>
+   
+
+  </div>
+</div>
 </template>
 <script>
+
+import DisplaySmall from "../Typography/DisplaySmall"
+
 export default {
-    name: 'LargeCard'
+    name: 'LargeCard',
+    components: {
+        DisplaySmall
+    },
+    props: {
+        img: String,
+        cardTitle: String,
+    }
 }
 </script>
+
+<style>
+.LargeCard{
+    width: 1000px;
+    height: 500px;
+}
+
+
+</style>
+
