6011077cb469b62629c962517511081bfe6837f7
amodinho <u14284783@tuks.co.za>
2019-05-13 16:28:03 +0200

    Added the image of PMB and the href

--- src/components/Typography/Copyright.vue
+++ src/components/Typography/Copyright.vue
@@ -1,6 +1,15 @@
 <template>
     <p class="text-sm font-body text-white pl-32">
-     &copy;{{new Date().getFullYear()}}
+     &copy;{{new Date().getFullYear()}}, a 
+     <a href="www.pimpmybook.co.za"
+     class="no-underline">
+     <img 
+     alt="PMB Logo"
+     class="h-10 pt-3"
+     src="https://s3.amazonaws.com/goa-assets/Images/PMB-Logo_No-slogen.png"
+     />
+     </a>
+     company
     </p>
 </template>
 <script>
