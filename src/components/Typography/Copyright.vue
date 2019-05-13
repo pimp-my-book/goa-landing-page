58f2bb51175aa7550a73e733a2c9b1c611e9e5fd
amodinho <u14284783@tuks.co.za>
2019-05-13 16:11:00 +0200

    Initialized the copywright component

---
+++ src/components/Typography/Copyright.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="text-sm font-body text-white">
+     
+    </p>
+</template>
+<script>
+export default {
+    name:'Copywright',
+    
+}
+</script>
+<style scoped>
+
+</style>
+
