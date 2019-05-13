8ba5519d7a821d77d2ca1c3f384264de75fca6de
amodinho <u14284783@tuks.co.za>
2019-05-13 16:36:20 +0200

    Useless changes

--- src/components/Typography/Credits.vue
+++ src/components/Typography/Credits.vue
@@ -1,5 +1,5 @@
 <template>
-    <p class="text-sm font-body text-white">
+    <p class="text-sm font-body text-white Credits">
      Made with ❤️ in <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-south-africa_1f1ff-1f1e6.svg"/>
      & <img src="https://s3.amazonaws.com/goa-assets/Icons/flag-for-india_1f1ee-1f1f3.svg"/>
     </p>
@@ -13,6 +13,10 @@ export default {
 <style scoped>
 img{
     height:20px;
+}
+
+.Credits{
+    left:10vw; 
 }
 </style>
 
