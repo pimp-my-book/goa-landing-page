11526d8bccda1675d70403d6712aaecf764b0745
amodinho <u14284783@tuks.co.za>
2019-05-16 12:12:19 +0200

    Added the contact form to the apge

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -5,15 +5,20 @@
        BodyText="Below is a contact form that you can fill out and send to us. Then someone will get in touch with you to help you with an queries."
        />
 
+<div class="container">
+    <ContactForm/>
+</div>
 </div>
 </template>
 <script>
 import BoarderHeader from '../components/Headers/BoarderHeader'
+import ContactForm from '../components/Forms/ContactForm'
 
 export default {
     name: 'Contact',
     components:{
-        BoarderHeader
+        BoarderHeader,
+        ContactForm
     }
 }
 </script>
