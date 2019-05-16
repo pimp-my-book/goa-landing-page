fb7ef51d0f71e575faa535e8fe9f765c39dff08c
amodinho <u14284783@tuks.co.za>
2019-05-16 12:17:07 +0200

    Iniliized display large compoent

--- src/components/Forms/ContactForm.vue
+++ src/components/Forms/ContactForm.vue
@@ -1,18 +1,24 @@
 <template>
-<form>
+<form class="w-full max-w-lg shadow-lg">
+    <div class="text-center">
+        <DisplaySmall
+        text="We'd Like To Get To Know You Better"
+        class="text-black"
+        />
+    </div>
 
 </form>
     
 </template>
 <script>
-import DisplayXLarge from '../Typography/DisplayXLarge'
+import DisplaySmall from '../Typography/DisplaySmall'
 import FeatureText from '../Typography/FeatureText'
 
 
 export default {
     name: 'ContactForm',
      components:{
-        DisplayXLarge,
+        DisplaySmall,
         FeatureText
     },
 }
