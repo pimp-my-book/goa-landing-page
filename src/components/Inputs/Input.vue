9e7dca4890d4917b2b65cfbce80da636182d19ee
amodinho <u14284783@tuks.co.za>
2019-05-16 12:51:53 +0200

    Added focus

--- src/components/Inputs/Input.vue
+++ src/components/Inputs/Input.vue
@@ -1,6 +1,6 @@
 <template>
     <input 
-    class="bg-grey-lighter rounded-px py-2 border-gray-500"
+    class="bg-grey-lighter rounded-px py-2 border-gray-500 focus:outline-none focus:bg-white"
     />
 </template>
 <script>
