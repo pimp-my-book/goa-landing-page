2ea9d62d138b178bcbe8fba25455dde3636afeba
amodinho <u14284783@tuks.co.za>
2019-05-16 11:56:37 +0200

    Added the compy into the components

--- src/components/Headers/BoarderHeader.vue
+++ src/components/Headers/BoarderHeader.vue
@@ -1,6 +1,13 @@
 <template>
-    <header class="w-full h-64 bg-green-gradient lg:rounded-header-border s:header-border-sm">
-
+    <header class="w-full h-64 bg-green-gradient text-center lg:rounded-header-border s:header-border-sm">
+   <DisplayXLarge
+   text="Looking For More Info?"
+   class="text-white pt-5"
+   />
+    <FeatureText
+    text="Below is a contact form that you can fill out and send to us. Then someone will get in touch with you to help you with an queries."
+    class="text-white pt-5"
+    />
     </header>
 </template>    
 <script> 
