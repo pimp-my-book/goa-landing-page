ff5c4775516fa6b101535ea52215f09be4cf218c
amodinho <u14284783@tuks.co.za>
2019-05-16 12:09:06 +0200

    Imported typography

--- src/components/Forms/ContactForm.vue
+++ src/components/Forms/ContactForm.vue
@@ -1,9 +1,20 @@
 <template>
+<form>
+
+</form>
     
 </template>
 <script>
+import DisplayXLarge from '../Typography/DisplayXLarge'
+import FeatureText from '../Typography/FeatureText'
+
+
 export default {
-    name: 'ContactForm'
+    name: 'ContactForm',
+     components:{
+        DisplayXLarge,
+        FeatureText
+    },
 }
 </script>
 
