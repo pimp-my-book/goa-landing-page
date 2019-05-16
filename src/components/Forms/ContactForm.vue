306832c7bef0c006a11967d160f0e54fe1a9d2c0
amodinho <u14284783@tuks.co.za>
2019-05-16 12:21:10 +0200

    Structuring the form

--- src/components/Forms/ContactForm.vue
+++ src/components/Forms/ContactForm.vue
@@ -6,6 +6,14 @@
         class="text-black"
         />
     </div>
+    <div class="flex flex-wrap -m3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        
+        </div>
+    </div>
 
 </form>
     
