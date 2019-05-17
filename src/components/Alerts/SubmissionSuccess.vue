7fd3772190691bab9bd638eec397ca24c00027f3
amodinho <u14284783@tuks.co.za>
2019-05-17 11:17:44 +0200

    Completed the submission Success

--- src/components/Alerts/SubmissionSuccess.vue
+++ src/components/Alerts/SubmissionSuccess.vue
@@ -1,9 +1,18 @@
 <template>
-    
+    <div>
+        <DisplayLarge
+        text="Thanks! Someone will get in touch with you!"
+        />
+    </div>
 </template>
 <script>
+import DisplayLarge from '../Typography/DisplayLarge'
+
 export default {
-    name: 'SubmissionSuccess'
+    name: 'SubmissionSuccess',
+    components: {
+        DisplayLarge
+    }
 }
 </script>
 
