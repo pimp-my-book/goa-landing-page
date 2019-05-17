52d66760d82aca9378347661443dbc865cc2d707
amodinho <u14284783@tuks.co.za>
2019-05-17 14:33:18 +0200

    Added the images

--- src/components/Headers/Companies.vue
+++ src/components/Headers/Companies.vue
@@ -5,7 +5,29 @@
      text="We Keep Great Company"
      />
     </div>
-
+    <div class="flex flex-row">
+     <img 
+     class=""
+     src="https://s3.amazonaws.com/goa-assets/Images/companies/AGOF-Logo.png"
+     alt="Alan Gray"
+     />
+     <img 
+     src="https://s3.amazonaws.com/goa-assets/Images/companies/Acc_Logo_Black_Purple_RGB.PNG"
+     alt="Accenture"
+     />
+     <img 
+     src="https://s3.amazonaws.com/goa-assets/Images/companies/MAD.png"
+     alt="MAD"
+     />
+     <img 
+     src="https://s3.amazonaws.com/goa-assets/Images/companies/Moshal_weloveIT_logo254-90.png"
+     alt="Moshal"
+     />
+     <img 
+     src="https://s3.amazonaws.com/goa-assets/Images/companies/crf-logo.jpg"
+     alt="Cyril Ramamphosa"
+     />
+    </div>
     </section>
 </template>
 <script>
