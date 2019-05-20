0ccf6892b9c800fe92a61bfe55a8a8accb4c9b68
amodinho <u14284783@tuks.co.za>
2019-05-20 14:49:44 +0200

    Added margin to the top

--- src/components/Headers/Companies.vue
+++ src/components/Headers/Companies.vue
@@ -1,33 +1,33 @@
 <template>
-    <section class="mt-10 pl-20" >
+    <section class="mt-10 lg:pl-20" >
     <div class="text-center mb-10">
      <DisplayLarge
      text="We Keep Great Company"
      />
     </div>
-    <div class="flex flex-row ">
+    <div class="flex lg:flex-row s:flex-col">
      <img 
-     class="h-90 mr-5"
+     class="h-90 mr-5 s:mt-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/AGOF-Logo.png"
      alt="Alan Gray"
      />
      <img 
-     class="h-90 mr-5"
+     class="h-90 mr-5 s:mt-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Acc_Logo_Black_Purple_RGB.PNG"
      alt="Accenture"
      />
      <img 
-     class="h-90 mr-5"
+     class="h-90 mr-5 s:mt-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/oasis.png"
      alt="Oasis"
      />
      <img 
-     class="h-90 mr-5"
+     class="h-90 mr-5 s:mt-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Moshal_weloveIT_logo254-90.png"
      alt="Moshal"
      />
      <img 
-     class="h-90 mr-5"
+     class="h-90 mr-5 s:mt-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/crf-logo.jpg"
      alt="Cyril Ramamphosa"
      />
