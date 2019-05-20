2fbc5fd2ff630750b69dfdc0c0a7795052ac62de
amodinho <u14284783@tuks.co.za>
2019-05-20 15:08:53 +0200

    Added the necessary padding for medium devices

--- src/components/Headers/Companies.vue
+++ src/components/Headers/Companies.vue
@@ -1,33 +1,33 @@
 <template>
-    <section class="mt-10 lg:pl-20 s:pl-5" >
+    <section class="mt-10 lg:pl-20 s:pl-5 md:pl-10" >
     <div class="text-center mb-10">
      <DisplayLarge
      text="We Keep Great Company"
      />
     </div>
-    <div class="flex lg:flex-row s:flex-col">
+    <div class="flex lg:flex-row s:flex-col ">
      <img 
-     class="h-90 mr-5 s:mt-5 md:w-3/4 lg:w-64" 
+     class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-200 lg:w-64" 
      src="https://s3.amazonaws.com/goa-assets/Images/companies/AGOF-Logo.png"
      alt="Alan Gray"
      />
      <img 
-     class="h-90 mr-5 s:mt-5 md:w-3/4 lg:w-64"
+     class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-150 lg:w-64"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Acc_Logo_Black_Purple_RGB.PNG"
      alt="Accenture"
      />
      <img 
-     class="h-90 mr-5 s:mt-5 md:w-3/4 md:h-full lg:w-64"
+     class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-250 lg:w-64"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/oasis.png"
      alt="Oasis"
      />
      <img 
-     class="h-90 mr-5 s:mt-5 md:w-3/4 lg:w-64"
+     class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-200 lg:w-64"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Moshal_weloveIT_logo254-90.png"
      alt="Moshal"
      />
      <img 
-     class="h-90 mr-5 s:mt-5 md:w-3/4 lg:w-64"
+     class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-200 lg:w-64"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/crf-logo.jpg"
      alt="Cyril Ramamphosa"
      />
