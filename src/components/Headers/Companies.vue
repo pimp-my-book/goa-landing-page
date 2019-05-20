65f3be3b661162ccaae207946a8a5607f907c791
amodinho <u14284783@tuks.co.za>
2019-05-20 14:44:18 +0200

    Added marging right

--- src/components/Headers/Companies.vue
+++ src/components/Headers/Companies.vue
@@ -7,27 +7,27 @@
     </div>
     <div class="flex flex-row ">
      <img 
-     class="h-90"
+     class="h-90 mr-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/AGOF-Logo.png"
      alt="Alan Gray"
      />
      <img 
-     class="h-90"
+     class="h-90 mr-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Acc_Logo_Black_Purple_RGB.PNG"
      alt="Accenture"
      />
      <img 
-     class="h-90"
+     class="h-90 mr-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/oasis.png"
      alt="Oasis"
      />
      <img 
-     class="h-90"
+     class="h-90 mr-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Moshal_weloveIT_logo254-90.png"
      alt="Moshal"
      />
      <img 
-     class="h-90"
+     class="h-90 mr-5"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/crf-logo.jpg"
      alt="Cyril Ramamphosa"
      />
