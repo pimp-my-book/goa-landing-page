e687f39401b4f01f6303d642abd37df33688f8b2
amodinho <u14284783@tuks.co.za>
2019-05-21 12:39:00 +0200

    Initilized failure image

--- src/components/Alerts/SubmissionFailure.vue
+++ src/components/Alerts/SubmissionFailure.vue
@@ -1,7 +1,16 @@
 <template>
-    <DisplayLarge
+<div class="container mx-auto my-auto pt-20">
+<DisplayLarge
         text="Whoops! Something is not working"
+        class="text-black mb-5"
         />
+         <img
+        src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_happy_feeling_slmw.svg"
+        alt="failure image"
+        class="w-img h-img"
+        />
+</div>
+    
 </template>
 <script>
 import DisplayLarge from '../Typography/DisplayLarge'
