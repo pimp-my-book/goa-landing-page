ccf3ff96ca1fa7033e1f5fc3878a23a1b51d717a
amodinho <u14284783@tuks.co.za>
2019-05-21 12:45:40 +0200

    Fixed typo

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const routes = [
 
     },
     {
-        path: '/SubmissonFailure',
+        path: '/SubmissionFailure',
         component: SubmissionFailure,
         name: 'SubmissionFailure'
     }
