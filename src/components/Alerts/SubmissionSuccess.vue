55f967c7bce4462062fe4b905be3051c41d32a54
amodinho <u14284783@tuks.co.za>
2019-05-21 12:38:04 +0200

    Fixed submission success

--- src/components/Alerts/SubmissionSuccess.vue
+++ src/components/Alerts/SubmissionSuccess.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container mx-auto my-auto">
+    <div class="container mx-auto my-auto pt-20">
         <DisplayLarge
         text="Thanks! Someone will get in touch with you!"
-        class="text-black"
+        class="text-black mb-5"
         />
         <img
         src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_happy_feeling_slmw.svg"
