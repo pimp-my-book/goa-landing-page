ae94dbb5fbaeea74cbf55ea46c168abeb393ade2
amodinho <u14284783@tuks.co.za>
2019-05-21 12:48:54 +0200

    Completed the 404

--- src/components/Bodies/404.vue
+++ src/components/Bodies/404.vue
@@ -1,8 +1,25 @@
 <template>
+<div class="container mx-auto my-auto pt-20">
+<DisplayLarge
+        text="Whatever you are looking is not here."
+        class="text-black mb-5"
+        />
+         <img
+        src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_page_not_found_su7k.svg"
+        alt="failure image"
+        class="w-img h-img"
+        />
+</div>
+    
     
 </template>
 <script>
+import DisplayLarge from '../Typography/DisplayLarge'
+
 export default {
-    name:'404'
+    name:'404',
+    components:{
+        DisplayLarge
+    }
 }
 </script>
