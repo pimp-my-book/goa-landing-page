432457cf6cd967f1c3babc3bae925aeeee865896
amodinho <u14284783@tuks.co.za>
2019-05-21 12:42:24 +0200

    Added the final touches to the fail submission

--- src/components/Alerts/SubmissionFailure.vue
+++ src/components/Alerts/SubmissionFailure.vue
@@ -5,7 +5,7 @@
         class="text-black mb-5"
         />
          <img
-        src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_happy_feeling_slmw.svg"
+        src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_feeling_blue_4b7q.svg"
         alt="failure image"
         class="w-img h-img"
         />
