9089d20fdd202e6eb98f29009db6c96ffe99e854
amodinho <u14284783@tuks.co.za>
2019-05-22 11:11:41 +0200

    Adde the border component

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -2,7 +2,12 @@
     <h1>How it works</h1>
 </template>
 <script>
+import BoarderHeader from '../components/Headers/BoarderHeader'
+
 export default {
-    name:'HowItWorks'
+    name:'HowItWorks',
+    components:{
+        BoarderHeader
+    }
 }
 </script>
