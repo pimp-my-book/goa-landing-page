8e5146e3384522e1f8a647c342ee29f0bd99ef97
amodinho <u14284783@tuks.co.za>
2019-05-22 11:43:04 +0200

    Added ahow it works body

---
+++ src/components/Bodies/HowBody.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container mx-auto">
+         <!--- start of row 1  -->
+        <!--- End of row 1  -->
+
+        <!--- start of row 2  -->
+        <!--- End of row 2  -->
+
+        <!--- start of row 3  -->
+        <!--- End of row 3  -->
+
+        <!--- start of row 4  -->
+        <!--- End of row 4  -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HowBody'
+}
+</script>
+
--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -5,17 +5,7 @@
        BodyText="As a student getting textbooks can be an irrtating task. Below we aim to simplify it."
        />
 
-       <!--- start of row 1  -->
-        <!--- End of row 1  -->
-
-        <!--- start of row 2  -->
-        <!--- End of row 2  -->
-
-        <!--- start of row 3  -->
-        <!--- End of row 3  -->
-
-        <!--- start of row 4  -->
-        <!--- End of row 4  -->
+      
        </div>
 </template>
 <script>
