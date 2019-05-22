f46fe1c611c78e43a11094fb5d0b6f8938d408bd
amodinho <u14284783@tuks.co.za>
2019-05-22 11:48:55 +0200

    Added the first bit of copy

--- src/components/Bodies/HowBody.vue
+++ src/components/Bodies/HowBody.vue
@@ -12,18 +12,18 @@
      <img 
      class="w-img h-img"
      alt="students" 
-     src=""
+     src="https://s3.amazonaws.com/goa-assets/Illustrations/undraw_hello_aeia.svg"
      />
    </div>
 <!-- End of img -->
 <!-- start of text-part of div -->
 <div class="lg:text-left lg:w-3/4 lg:pl-32 s:text-center">
      <DisplaySmall
-   text=""
+   text="Your Students Sign Up"
    class="mb-5 text-blue-dark"
    />
    <FeatureText
-   text=""
+   text="Your students simply enter their details and sign up for GOA."
    />
    </div>
      <!-- End of text-part of div -->
