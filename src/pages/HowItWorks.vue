9557d7a83cc576ee2ff662adefd77aba8a1b9777
amodinho <u14284783@tuks.co.za>
2019-05-22 11:36:38 +0200

    Added a div and comments

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -1,8 +1,22 @@
 <template>
+       <div>
     <BoarderHeader
        HeaderText="Need More Clarity?"
        BodyText="As a student getting textbooks can be an irrtating task. Below we aim to simplify it."
        />
+
+       <!--- start of row 1  -->
+        <!--- End of row 1  -->
+
+        <!--- start of row 2  -->
+        <!--- End of row 2  -->
+
+        <!--- start of row 3  -->
+        <!--- End of row 3  -->
+
+        <!--- start of row 4  -->
+        <!--- End of row 4  -->
+       </div>
 </template>
 <script>
 import BoarderHeader from '../components/Headers/BoarderHeader'
