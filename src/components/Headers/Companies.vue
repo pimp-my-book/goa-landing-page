4a80ab9050ee2cd3312cccb14a5335f705ea6438
amodinho <u14284783@tuks.co.za>
2019-05-22 10:44:09 +0200

    Yoh

--- src/components/Headers/Companies.vue
+++ src/components/Headers/Companies.vue
@@ -10,7 +10,7 @@
      class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-200 lg:w-64" 
      src="https://s3.amazonaws.com/goa-assets/Images/companies/AGOF-Logo.png"
      alt="Alan Gray"
-     />
+     /> 
      <img 
      class="lg:h-90 mr-5 s:mt-5 md:w-3/4 md:h-150 lg:w-64"
      src="https://s3.amazonaws.com/goa-assets/Images/companies/Acc_Logo_Black_Purple_RGB.PNG"
