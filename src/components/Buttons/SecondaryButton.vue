4ea64c16f89a0105bb7d3f88729ca01a46b948ab
amodinho <u14284783@tuks.co.za>
2019-05-27 16:33:07 +0200

    Fized focus for the secondary button

--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -3,8 +3,8 @@
     type="submit"
     @click="forward"
     :class="[regular 
-    ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-darker   rounded' 
-    : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6  s:py-2 s:px-2 text-green-darker rounded']">
+    ? ' hover:text-green-darker hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-green-darker   rounded focus:outline-none' 
+    : ' hover:text-green-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6  s:py-2 s:px-2 text-green-darker rounded focus:outline-none']">
                 
      
      {{text}}
