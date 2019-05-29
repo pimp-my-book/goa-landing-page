7aad28d84e7c1f62788cddc9c3854b1ccc40698f
amodinho <u14284783@tuks.co.za>
2019-05-29 11:35:22 +0200

    Added all the content for the accordian

--- src/components/Navigation/Accordian.vue
+++ src/components/Navigation/Accordian.vue
@@ -5,6 +5,18 @@
         <section id="content1">
             <p>Answer to question</p>
         </section>
+
+        <input id="toggle2" type="radio" name="toggle"/>
+        <label for="toggle2">Question 1</label>
+        <section id="content2">
+            <p>Answer to question</p>
+        </section>
+
+        <input id="toggle3" type="radio" name="toggle"/>
+        <label for="toggle3">Question 1</label>
+        <section id="content3">
+            <p>Answer to question</p>
+        </section>
     </div>
 </template>
 <script>
