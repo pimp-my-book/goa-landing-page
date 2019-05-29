e8a9cbbcfbfdbb89945b2e4e5767ced6a24e4d34
amodinho <u14284783@tuks.co.za>
2019-05-29 11:18:43 +0200

    Busy with the accordian

--- src/components/Navigation/Accordian.vue
+++ src/components/Navigation/Accordian.vue
@@ -1,5 +1,11 @@
 <template>
-    
+    <div>
+        <input id="toggle1" type="radio" name="toggle"/>
+        <label for="toggle1">Question 1</label>
+        <section id="content1">
+            <p>Answer to question</p>
+        </section>
+    </div>
 </template>
 <script>
 export default {
