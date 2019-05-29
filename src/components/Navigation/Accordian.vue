af9d76894bd7b006fca9754eaee417430fc031d2
amodinho <u14284783@tuks.co.za>
2019-05-29 11:51:33 +0200

    Added the feature text component

--- src/components/Navigation/Accordian.vue
+++ src/components/Navigation/Accordian.vue
@@ -1,27 +1,44 @@
 <template>
     <div class="bg-white shadow-md w-full text-left">
         <input id="toggle1" type="radio" name="toggle"/>
-        <label for="toggle1">Question 1</label>
+        <label for="toggle1">
+            <FeatureText
+            text="Question 1"
+            />
+        </label>
         <section id="content1">
             <p>Answer to question</p>
         </section>
 
         <input id="toggle2" type="radio" name="toggle"/>
-        <label for="toggle2">Question 1</label>
+        <label for="toggle2">
+            <FeatureText
+            text="Question 2"
+            />
+        </label>
         <section id="content2">
             <p>Answer to question</p>
         </section>
 
         <input id="toggle3" type="radio" name="toggle"/>
-        <label for="toggle3">Question 1</label>
+        <label for="toggle3">
+           <FeatureText
+            text="Question 3"
+            />
+            </label>
         <section id="content3">
             <p>Answer to question</p>
         </section>
     </div>
 </template>
 <script>
+import FeatureText from '../Typography/FeatureText'
+
 export default {
-    name: 'Accordian'
+    name: 'Accordian',
+    components:{
+        FeatureText
+    }
 }
 </script>
 <style scoped>
