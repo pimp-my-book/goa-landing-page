53ec85c65aafa337852aab039099852a2b51e120
amodinho <u14284783@tuks.co.za>
2019-06-24 15:47:24 +0200

    Added vuebootstrap

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
 import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstap-vue/dist/bootstrap-vue.css'
 import router from './router'
 Vue.config.productionTip = false
 
 const isProd = process.env.NODE_ENV === 'production'
 
+Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 Vue.use(VueAnalytics, {
 
