1679a781ff956999a786ae252fade2a5fea3af48
amodinho <u14284783@tuks.co.za>
2019-06-25 11:36:07 +0200

    Added typography components

--- src/components/Headers/BuyBackHeader.vue
+++ src/components/Headers/BuyBackHeader.vue
@@ -1,8 +1,17 @@
 <template>
-    
+    <header>
+
+    </header>
 </template>
 <script>
+import DisplayXLarge from "../Typography/DisplayXLarge"
+import FeatureText from "../Typography/FeatureText"
+
 export default {
-    name:'BuyBackHeader'
+    name:'BuyBackHeader',
+    components:{
+        DisplayXLarge,
+        FeatureText
+    }
 }
 </script>
