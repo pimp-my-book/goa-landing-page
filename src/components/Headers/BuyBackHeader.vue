8b7f90b361eea3b2e9748a4d733e67c6e39d0c10
amodinho <u14284783@tuks.co.za>
2019-06-25 11:37:33 +0200

    Added the text for the typography components

--- src/components/Headers/BuyBackHeader.vue
+++ src/components/Headers/BuyBackHeader.vue
@@ -1,6 +1,11 @@
 <template>
     <header>
-
+     <DisplayXLarge
+     text="Sell Back your products"
+     />
+     <FeatureText
+     text="Products such as textbooks are normally only used for a particular course in a semester. We are able to manage on your behalf the collection and resale of those books. We will first try to re-supply them to your own students and then we will sell them through our partner vendor Pimp My Book."
+     />
     </header>
 </template>
 <script>
