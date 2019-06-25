3345493a19a66484fb4562457b37882b7c74b62f
amodinho <u14284783@tuks.co.za>
2019-06-25 10:34:53 +0200

    Added the collapse component

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueAnalytics from 'vue-analytics'
+import {Collapse} from 'buefy/dist/components/collapse'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import 'buefy/dist/buefy.css'
 
 import router from './router'
 Vue.config.productionTip = false
 
 const isProd = process.env.NODE_ENV === 'production'
 
+
+Vue.component('b-collapse', Collapse)
 Vue.use(VueRouter)
 Vue.use(VueAnalytics, {
 
