1d3cf4aee5ab9aa08f7e19c1e7bc894b0c9ccfe7
amodinho <u14284783@tuks.co.za>
2019-07-09 13:53:04 +0200

    added how it works comments

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -1,11 +1,15 @@
 <template>
        <div>
+         <!-- boarder header -->
     <BoarderHeader
        HeaderText="Need More Clarity?"
        BodyText="As a student getting textbooks can be an irritating task. Below we aim to simplify it."
        />
+       <!-- the how it works body content -->
 <HowBody/>
       <div class="flex justify-center mt-20 lg:pl-32 ">
+
+        <!--  a large card to contact sales-->
 <LargeCard
 img="https://s3.amazonaws.com/goa-assets/Icons/1.+Envelope+opened.svg"
 cardTitle="Get In Touch With Us"
