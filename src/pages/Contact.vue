8c98bbcdea4c1236db3c2bfc4e5fde3f44c49c34
amodinho <u14284783@tuks.co.za>
2019-07-09 13:51:54 +0200

    Added comments for contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,10 +1,12 @@
 <template>
 <div>
+
+  <!-- Boarder header -->
        <BoarderHeader
        HeaderText="Looking For More Info?"
        BodyText="Below is a contact form that you can fill out and send to us. Then someone will get in touch with you to help you with an queries."
        />
-
+<!-- contact form  -->
 <div class="container lg:py-10 lg:px-10 s:px-5 mx-auto">
     <div class="lg:flex lg:justify-center">
       <ContactForm/>
