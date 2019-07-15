fbaa8b660aea1980fe4f95340ddd14fa9cd026da
amodinho <u14284783@tuks.co.za>
2019-07-15 10:51:03 +0200

    added components

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -3,7 +3,17 @@
 </template>
 
 <script>
+import BoarderHeader from '../components/Headers/BoarderHeader'
+import DisplayLarge from '../components/Typography/DisplayLarge'
+import FeatureText from '../components/Typography/FeatureText'
+
+
 export default {
-    name: 'Privacy'
+    name: 'Privacy',
+    components:{
+        BoarderHeader,
+        DisplayLarge,
+        FeatureText
+    }
 }
 </script>
