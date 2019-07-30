59b25e13cf0d1ac20815cb51ce85d78b86c30f0d
amodinho <u14284783@tuks.co.za>
2019-07-30 12:03:02 +0200

    added text

--- src/pages/Explainer.vue
+++ src/pages/Explainer.vue
@@ -74,6 +74,19 @@ class=" xl:text-2xl"
 
 
 <div>
+
+
+    <div>
+    
+<FeatureText
+class="p-4 xl:text-2xl"
+        text="Run over your order one more time to make sure your order is correct, if not, simply click “Cancel” . If all is in order and ready to go, click “Place Order”"
+        />
+         <img
+        src="https://goa-assets.s3.amazonaws.com/Images/tutorials/place_order_step_two.svg"
+        class="lg:ml-12 "
+        />
+    </div>
     
 <FeatureText
 class=" xl:text-2xl"
