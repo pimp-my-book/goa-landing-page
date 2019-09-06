36aa6aa705e09dc7b00f2ad6dc180c566a6a1218
AmoDinho <u14284783@tuks.co.za>
2019-09-06 11:10:21 +0200

    added image to header

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>ABOUT</div>
+  <header>
+    <img
+      alt="library of books"
+      class="w-full"
+      src="https://images.unsplash.com/photo-1431608660976-4fe5bcc2112c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+    />
+  </header>
 </template>
 <script>
 export default {
-    name:'About'
-}
+  name: "About"
+};
 </script>
