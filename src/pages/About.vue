4dde482cf4a859d9365cf5167e898e4a1bae954b
AmoDinho <u14284783@tuks.co.za>
2019-09-06 11:23:29 +0200

    added background image

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <header class="h-250">
-      <img
-        alt="library of books"
-        class="w-full"
-        src="https://images.unsplash.com/photo-1431608660976-4fe5bcc2112c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-      />
-    </header>
-    <div></div>
+    <header :style="styles" class></header>
+    <div class="p-20">
+      <DisplayLarge text="Our Story" />
+    </div>
   </div>
 </template>
 <script>
+import DisplayLarge from "../components/Typography/DisplayLarge";
+
 export default {
-  name: "About"
+  name: "About",
+  components: {
+    DisplayLarge
+  },
+  computed: {
+    styles() {
+      return {
+        "background-image": `url('https://images.unsplash.com/photo-1431608660976-4fe5bcc2112c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')`
+      };
+    }
+  }
 };
 </script>
