a1acdfb81674850d83c7db81457a7456d8d5b135
AmoDinho <u14284783@tuks.co.za>
2019-09-06 11:11:27 +0200

    addded new div

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="h-250">
-    <img
-      alt="library of books"
-      class="w-full"
-      src="https://images.unsplash.com/photo-1431608660976-4fe5bcc2112c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-    />
-  </header>
+  <div>
+    <header class="h-250">
+      <img
+        alt="library of books"
+        class="w-full"
+        src="https://images.unsplash.com/photo-1431608660976-4fe5bcc2112c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+      />
+    </header>
+    <div></div>
+  </div>
 </template>
 <script>
 export default {
