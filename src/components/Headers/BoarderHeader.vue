2d0a1c6fbd853d49f10c643a2a0810695a50ceca
AmoDinho <u14284783@tuks.co.za>
2019-10-29 10:12:25 +0200

    made a change to the height of the header

--- src/components/Headers/BoarderHeader.vue
+++ src/components/Headers/BoarderHeader.vue
@@ -1,37 +1,31 @@
 <template>
-    <header class="w-full lg:h-64 s:h-250 sl:h-250 bg-green-gradient text-center lg:rounded-header-border s:header-border-sm">
-   <DisplayXLarge
-   :text="HeaderText"
-   class="text-white pt-5"
-   />
-    <FeatureText
-    :text="BodyText"
-    class="text-black pt-5 lg:px-32   xl:text-3xl"
-    />
-    </header>
+  <header
+    class="w-full lg:h-64 s:h-250 sl:h-350 bg-green-gradient text-center lg:rounded-header-border s:header-border-sm"
+  >
+    <DisplayXLarge :text="HeaderText" class="text-white pt-5" />
+    <FeatureText :text="BodyText" class="text-black pt-5 lg:px-32 xl:text-3xl" />
+  </header>
 </template>    
-<script> 
-import DisplayXLarge from '../Typography/DisplayXLarge'
-import FeatureText from '../Typography/FeatureText'
+<script>
+import DisplayXLarge from "../Typography/DisplayXLarge";
+import FeatureText from "../Typography/FeatureText";
 
 export default {
-    name: 'BoarderHeader',
-    components:{
-        DisplayXLarge,
-        FeatureText
-    },
-    props: {
-        HeaderText: String,
-        BodyText: String
-    }
-}
+  name: "BoarderHeader",
+  components: {
+    DisplayXLarge,
+    FeatureText
+  },
+  props: {
+    HeaderText: String,
+    BodyText: String
+  }
+};
 </script>
 
 <style scoped>
-header{
-    background: linear-gradient(179.9deg, #A0FFAB 23.23%, #94B5E3 99.68%);
+header {
+  background: linear-gradient(179.9deg, #a0ffab 23.23%, #94b5e3 99.68%);
 }
-
-
 </style>
 
