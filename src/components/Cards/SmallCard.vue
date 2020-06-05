17e1368d8b6c4e050292832532110565ddc06039
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-05 02:10:11 +0200

    refactor: fixed image and use embed instead of img

--- src/components/Cards/SmallCard.vue
+++ src/components/Cards/SmallCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shadow-lg lg:w-800 lg:h-500 s:w-4/5 rounded-lg lg:mr-10">
-    <img class="mt-5" :src="img" />
+    <embed width="100px" height="100px" class="mt-5" :src="img" />
     <div>
       <DisplaySmall class="mt-5 text-blue-dark" :text="cardTitle" />
     </div>
