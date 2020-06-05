8ee16f3f53fc99f74f408f3010baac86577b68d7
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-05 03:48:25 +0200

    feat: IconGrid component

---
+++ src/components/Grids/IconGrid.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-4">
+            <div>
+                1
+            </div>
+            <div>
+                2
+            </div>
+            <div>
+                3
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IconGrid"
+};
+</script>
+
+<style>
+
+</style>
