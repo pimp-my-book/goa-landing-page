0093acb9afcd59ab6794e1f4a884e2bffad11d59
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-08 00:33:12 +0200

    refactor: HowItWorks page + update content

--- src/pages/Explainer.vue
+++ src/pages/Explainer.vue
@@ -9,12 +9,10 @@ this component houses a tut on how to palce an  order on GOA.
   <div>
     <div class="flex flex-col justify-center mt-5 mb-10">
       <DisplayLarge class="xl:text-4xl" text="How It Works" />
-      <FeatureText
-        class="xl:text-2xl"
-        text=""
-      />
+      <FeatureText class="xl:text-2xl" text />
     </div>
-    <HowToGrid v-bind:links='[
+    <HowToGrid
+      v-bind:links='[
         {
             url: "https://ask-thuto-assets.s3.amazonaws.com/how_to_order/01_handshake.svg",
             header: "1.	Meet new client (Online too)"
@@ -51,7 +49,51 @@ this component houses a tut on how to palce an  order on GOA.
             url: "https://ask-thuto-assets.s3.amazonaws.com/how_to_order/09_askthuto_pays_vendor.svg",
             header: "9. Askthuto pays Vendor"
         }
-    ]'/>
+    ]'
+    />
+    <div class="flex flex-col justify-center mt-10 mb-10">
+      <DisplayLarge class="xl:text-4xl" text="Cost of using AskThuto" />
+    </div>
+    <div class="cost-grid-container sm:flex lg:flex-row sm:flex-col content-center mx-auto">
+      <div class="cost-grid-item mx-auto text-lg rounded-lg shadow-lg p-6">
+        <p class="leading-normal text-left">
+          <strong>Platform Fee</strong><br/>
+          R600 - R1000 per student per annum<br/>
+          <br/>
+          <strong>Includes</strong><br/>
+          <ul class="mb-4">
+            <li>Delivery nationwide</li>
+            <li>Unlimited use of platform</li>
+            <li>Controls to track and review activity</li>
+            <li>Expenditure reports</li>
+            <li>Saving on overall budget (removing wasteful expenditure)</li>
+          </ul>
+        </p>
+          <div class="py-3 divider-line"></div>
+        <strong>Success Fee</strong><br/>
+        % of saving from waste reduction
+      </div>
+      <div class="cost-grid-item mx-auto text-lg rounded-lg shadow-lg p-6">
+        <p class="leading-normal text-left">
+          <strong>Sourcing Fee</strong><br/>
+          Cost from Publisher/Supplier + Markup<br/>
+          <br/>
+          <strong>Includes</strong><br/>
+          <ul class="mb-4">
+            <li>Bulk Discount</li>
+          </ul>
+        </p>
+          <div class="py-3 divider-line"></div>
+        <p class="leading-normal text-left">
+          <strong>Sourcing Cost 3rd Party</strong><br/>
+          Retail selling price of 3rd party vendor<br/>
+          <ul>
+            <li>Control even when using 3rd party stores</li>
+            <li>No admin cost of dealing with multiple stores</li>
+          </ul>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -68,3 +110,52 @@ export default {
   }
 };
 </script>
+
+<style>
+  ul li {
+    list-style-image: url("https://s3.amazonaws.com/goa-assets/Icons/Group+48.1.svg");
+  }
+
+  .cost-grid-container {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 10px;
+    padding: 10px;
+    margin: auto;
+    padding: 20px;
+    max-width: 1000px;
+  }
+
+  .cost-grid-container > div {
+    text-align: center;
+  }
+
+  .cost-grid-item {
+    width: 100%;
+    height: 100%;
+    max-width: 330px;
+    max-height: 695px
+  }
+
+  .divider-line {
+    border-top: 2px solid #000;
+    width: 80%;
+    margin: auto;
+    opacity: 0.4
+  }
+
+  @media screen and (max-width: 420px) {
+    .cost-grid-container {
+      grid-template-columns: auto
+    }
+    .cost-grid-item {
+      padding-top: 50px
+    }
+  }
+
+  @media screen and (max-width: 770px) and (min-width: 430px) {
+    .cost-grid-container {
+      grid-template-columns: auto auto;
+    }
+  }
+</style>
