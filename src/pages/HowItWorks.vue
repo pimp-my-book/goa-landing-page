c5eb094e4b59e7d0c36a0a668e9af93cdfa6ace5
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-07 17:23:03 +0200

    chore: reformat

--- src/pages/HowItWorks.vue
+++ src/pages/HowItWorks.vue
@@ -1,42 +1,39 @@
 <template>
-       <div>
-         <!-- boarder header -->
+  <div>
+    <!-- boarder header -->
     <BoarderHeader
-       HeaderText="Need More Clarity?"
-       BodyText="As a student getting textbooks can be an irritating task. Below we aim to simplify it."
-       />
-       <!-- the how it works body content -->
-<HowBody/>
-      <div class="flex justify-center mt-20 lg:pl-32 ">
-
-        <!--  a large card to contact sales-->
-<LargeCard
-img="https://s3.amazonaws.com/goa-assets/Icons/1.+Envelope+opened.svg"
-cardTitle="Get In Touch With Us"
-buttonURL="/contact"
-primaryText="Contact Sales"
-
-/>
-
-</div>
-       </div>
+      HeaderText="Need More Clarity?"
+      BodyText="As a student getting textbooks can be an irritating task. Below we aim to simplify it."
+    />
+    <!-- the how it works body content -->
+    <HowBody />
+    <div class="flex justify-center mt-20 lg:pl-32">
+      <!--  a large card to contact sales-->
+      <LargeCard
+        img="https://s3.amazonaws.com/goa-assets/Icons/1.+Envelope+opened.svg"
+        cardTitle="Get In Touch With Us"
+        buttonURL="/contact"
+        primaryText="Contact Sales"
+      />
+    </div>
+  </div>
 </template>
 <script>
-import BoarderHeader from '../components/Headers/BoarderHeader'
-import HowBody from '../components/Bodies/HowBody'
-import LargeCard from '../components/Cards/LargeCard'
+import BoarderHeader from "../components/Headers/BoarderHeader";
+import HowBody from "../components/Bodies/HowBody";
+import LargeCard from "../components/Cards/LargeCard";
 
 export default {
-    name:'HowItWorks',
-    components:{
-        BoarderHeader,
-        HowBody,
-        LargeCard
-    },
-    methods: {
-    track(){
-      this.$ga.page('/how-it-works')
+  name: "HowItWorks",
+  components: {
+    BoarderHeader,
+    HowBody,
+    LargeCard
+  },
+  methods: {
+    track() {
+      this.$ga.page("/how-it-works");
     }
   }
-}
+};
 </script>
