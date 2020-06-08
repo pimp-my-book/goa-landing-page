1784ba22934d9a1653e40b4a0dca4cdfdecb7588
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-08 10:25:10 +0200

    fix: SmallCard mobile render

--- src/components/Cards/SmallCard.vue
+++ src/components/Cards/SmallCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="shadow-lg lg:w-800 lg:h-400 s:w-4/5 rounded-lg lg:mr-10">
+  <div class="small-card-container shadow-lg lg:w-800 lg:h-400 s:w-4/5 rounded-lg lg:mr-5">
     <embed width="100px" height="100px" class="mt-5" :src="img" />
     <div>
-      <DisplaySmall class="mt-5 text-blue-dark" :text="cardTitle" />
+      <DisplaySmall class="p-2 mt-5 text-blue-dark" :text="cardTitle" />
     </div>
     <div>
-      <FeatureText class="mt-5 p-3 xl:text-2xl" :text="cardText" small />
+      <FeatureText class="mt-5 p-4 xl:text-2xl" :text="cardText" small />
     </div>
   </div>
 </template>
@@ -27,3 +27,12 @@ export default {
   }
 };
 </script>
+
+<style>
+@media screen and (max-width: 770px) {
+  .small-card-container {
+    margin: 0px;
+    margin-bottom: 30px;
+  }
+}
+</style>
