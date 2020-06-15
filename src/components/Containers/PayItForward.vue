5337832a5e6b8d36910e7c938ae1691ecceccd4e
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-06-15 11:44:17 +0200

    update: PayItForward

--- src/components/Containers/PayItForward.vue
+++ src/components/Containers/PayItForward.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="pay-it-forward-container">
         <img src="https://ask-thuto-assets.s3.amazonaws.com/home/pay-it-forward/head.jpg" alt="pay-it-forward"/>
-        <DisplaySmall text="If you can't Pay It Back, Pay It Forward" class="p-2 mb-5 text-left text-blue-dark" />
+        <DisplaySmall text="If you can't Pay It Back, Pay It Forward" class="p-2 mt-5 mb-5 text-left text-blue-dark" />
         <p class="text-xl text-center">
             Your students get so much from you, give them an opportunity to give back. We take back the used textbooks that your students no longer need. We either resupply them to your own students who need them or sell them on your behalf. Your students can help you to help more students. 
         </p>
