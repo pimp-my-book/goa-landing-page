ab5cbd0a9af194348ef5bb03a61ee7cd55598a08
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-08-13 06:33:53 +0200

    update: secondary-button variations

--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -5,7 +5,7 @@
     :class="[regular 
 
     ? 'hover:text-white hover:bg-transparent bg-green-lighter font-body py-2 px-4 text-ablue-darker rounded hover:border border-green hover:border-green focus:outline-none' 
-    : 'hover:text-white  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 hover:border hover:border-green s:py-2 s:px-2 text-ablue-darker rounded focus:outline-none']"
+    : 'hover:text-ablue-darker  hover:bg-transparent bg-green-lighter font-body lg:py-4 lg:px-6 sm:py-4 sm:px-6 hover:border hover:border-green s:py-2 s:px-2 text-ablue-darker rounded focus:outline-none']"
 
   >{{text}}</button>
 </template>
