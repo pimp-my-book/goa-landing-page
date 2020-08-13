a04f98210cbf3bf90a8498bd084d6cb5cb1cae25
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-08-13 05:46:59 +0200

    update: border-top color

--- tailwind.js
+++ tailwind.js
@@ -999,7 +999,7 @@ module.exports = {
     function({addComponents}){
       const boarders = {
         ".border-top": {
-          borderTop: "#4ECA5D 8px solid"
+          borderTop: "#ffffff 8px solid"
         }
       };
       addComponents(boarders);
