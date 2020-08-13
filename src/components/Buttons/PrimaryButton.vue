7b7afbabc6a921f537916cf08ec8c93d987fc85e
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-08-13 05:47:15 +0200

    update: primary-button color

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -17,8 +17,8 @@ forward - redirects using the url prop
     type="submit"
     url
     @click="forward"
-    :class="[regular ? ' bg-green hover:text-green-dark hover:bg-green-lighter font-body py-2 px-4  text-white rounded focus:bg-green-lighter focus:outline-none' 
-    : 'bg-green   hover:text-green-dark hover:bg-green-lighter font-body py-4 px-6 sl:py-4 sl:px-6 s:py-2 s:px-2 text-white rounded focus:outline-none ']"
+    :class="[regular ? 'bg-ablue-light hover:white hover:bg-ablue-lighter font-body py-2 px-4  text-white rounded focus:bg-green-lighter focus:outline-none' 
+    : 'bg-ablue  hover:white hover:bg-ablue-lighter font-body py-4 px-6 sl:py-4 sl:px-6 s:py-2 s:px-2 text-white rounded focus:outline-none']"
   >{{text}}</button>
 </template>
 
